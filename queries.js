59c4503f6cb61af71eb49908c5c9ac6364e38eb4
Charu Thomas <charu.thomas@gmail.com>
2018-02-27 06:01:26 -0500

    debug logs for get request

--- queries.js
+++ queries.js
@@ -9,18 +9,8 @@ var pgp = require('pg-promise')(options);
 var connectionString = 'postgres://localhost:5432/rf_bulk_pick_report';
 var db = pgp(connectionString);
 
-// add query functions
-
-module.exports = {
-  getAllItems: getAllItems,
-  getSingleItem: getSingleItem,
-  createItem: createItem,
-  updateItem: updateItem,
-  removeItem: removeItem
-};
-
 function getAllItems(req, res, next) {
-  db.many('select * from unit_of_work')
+  db.any('select * from unit_of_work')
     .then(function (data) {
       res.status(200)
         .json({
@@ -33,3 +23,11 @@ function getAllItems(req, res, next) {
       return next(err);
     });
 }
+
+module.exports = {
+  getAllItems: getAllItems,
+  getSingleItem: getSingleItem,
+  createItem: createItem,
+  updateItem: updateItem,
+  removeItem: removeItem
+};
